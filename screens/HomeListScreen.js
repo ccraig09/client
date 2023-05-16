87cf2fabf2ca0cfc55c2a798fffb0f2116825fab
Carlos <carlos.craig09@gmail.com>
2023-05-16 09:58:24 -0700

    added floating button

--- screens/HomeListScreen.js
+++ screens/HomeListScreen.js
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { FloatingAction } from "react-native-floating-action";
 
-const HomeListScreen = () => {
+import Card from "../components/Card";
+
+const HomeListScreen = (props) => {
 	return (
-		<View>
-			<Text>HomeListScreen</Text>
+		<View style={styles.container}>
+			<Card navigation={props.navigation} />
+			<FloatingAction
+				position="right"
+				animated={false}
+				showBackground={false}
+				onPressMain={() => props.navigation.navigate("AddHome")}
+			/>
 		</View>
 	);
 };
 
 export default HomeListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
